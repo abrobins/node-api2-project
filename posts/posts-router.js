bd77ad6c29eaa5b6ef013be78c870d31be647103
Blair <abrgroup@gmail.com>
2020-04-12 19:44:05 -0400

    MVP complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,9 +4,9 @@ const posts = require(".././data/db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(req.query);
+  console.log("req.query", req.query);
   posts
-    .find(req.query)
+    .find()
     .then(post => {
       res.status(200).json(post);
     })
@@ -130,6 +130,32 @@ router.delete("/:id", (req, res) => {
       console.log(error);
       res.status(500).json({
         error: "The post could not be removed"
+      });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+
+  posts
+    .update(req.params.id, req.body)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be modified."
       });
     });
 });
